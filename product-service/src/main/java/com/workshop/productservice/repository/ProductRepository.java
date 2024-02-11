0da626fcf45016915963b383b8edd400f1e53257
Giga Amiridze <giga.amiridze@azry.com>
2024-02-11 10:41:27 +0400

    Added product repository

---
+++ product-service/src/main/java/com/workshop/productservice/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.workshop.productservice.repository;
+
+import com.workshop.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
